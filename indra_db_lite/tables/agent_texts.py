8b5fb7826bb8b369757b5aa0e8420ffa27a37b86
Ubuntu <ubuntu@ip-172-31-22-103.ec2.internal>
2021-09-17 04:29:17 +0000

    Begin work to get agent texts table

---
+++ indra_db_lite/tables/agent_texts.py
@@ -0,0 +1,129 @@
+from contextlib import closing
+import logging
+import sqlite3
+
+from indra_db_lite.csv import query_to_csv, import_csv_into_sqlite
+
+logger = logging.getLogger(__name__)
+
+
+def agent_text_stmts_to_csv(outpath: str) -> None:
+    query = """
+    SELECT
+        id, db_id, stmt_id
+    FROM
+        raw_agents
+    WHERE
+        db_name = 'TEXT' AND
+        stmt_id IS NOT NULL
+    """
+    query_to_csv(query, outpath)
+
+
+def stmts_readings_to_csv(outpath: str) -> None:
+    query = """
+    SELECT
+        id, reading_id
+    FROM
+        raw_statements
+    WHERE
+        reading_id IS NOT NULL
+    """
+    query_to_csv(query, outpath)
+
+
+def readings_content_to_csv(outpath: str) -> None:
+    query = """
+    SELECT
+        id, text_content_id
+    FROM
+        reading
+    """
+    query_to_csv(query, outpath)
+
+
+def content_text_refs_to_csv(outpath: str) -> None:
+    query = """
+    SELECT
+        id, text_ref_id
+    FROM
+        text_content
+    """
+    query_to_csv(query, outpath)
+
+
+def create_temp_agent_text_tables(
+        agent_stmts_path: str,
+        stmt_readings_path: str,
+        reading_content_path: str,
+        content_text_refs_path: str,
+        sqlite_db_path: str,
+) -> None:
+    query1 = \
+        """--
+        CREATE TABLE IF NOT EXISTS agent_stmts (
+        id INTEGER PRIMARY KEY,
+        agent_text TEXT,
+        stmt_id INTEGER
+        );
+        """
+    query2 = \
+        """--
+        CREATE TABLE IF NOT EXISTS stmt_readings (
+        stmt_id INTEGER PRIMARY KEY,
+        reading_id INTEGER
+        );
+        """
+    query3 = \
+        """--
+        CREATE TABLE IF NOT EXISTS reading_content (
+        reading_id INTEGER PRIMARY KEY,
+        text_content_id INTEGER
+        );
+        """
+    query4 = \
+        """--
+        CREATE TABLE IF NOT EXISTS content_text_refs (
+        text_content_id INTEGER PRIMARY KEY,
+        text_ref_id INTEGER
+        );
+        """
+    with closing(sqlite3.connect(sqlite_db_path)) as conn:
+        with closing(conn.cursor()) as cur:
+            for query in query1, query2, query3, query4:
+                cur.execute(query)
+        conn.commit()
+    import_csv_into_sqlite(agent_stmts_path, 'agent_stmts', sqlite_db_path)
+    import_csv_into_sqlite(stmt_readings_path, 'stmt_readings', sqlite_db_path)
+    import_csv_into_sqlite(
+        reading_content_path, 'reading_content', sqlite_db_path
+    )
+    import_csv_into_sqlite(
+        content_text_refs_path, 'content_text_refs', sqlite_db_path
+    )
+
+
+def add_indices_to_temp_agent_text_tables(sqlite_db_path: str) -> None:
+    query1 = """--
+    CREATE INDEX IF NOT EXISTS
+        agent_stmts_stmt_id_idx
+    ON
+        agent_stmts(stmt_id)
+    """
+    query2 = """--
+    CREATE INDEX IF NOT EXISTS
+        stmt_readings_reading_id_idx
+    ON
+        stmt_readings(reading_id)
+    """
+    query3 = """--
+    CREATE INDEX IF NOT EXISTS
+        reading_content_text_content_id_idx
+    ON
+        stmt_readings(reading_id)
+    """
+    with closing(sqlite3.connect(sqlite_db_path)) as conn:
+        with closing(conn.cursor()) as cur:
+            for query in query1, query2, query3:
+                cur.execute(query)
+        conn.commit()
